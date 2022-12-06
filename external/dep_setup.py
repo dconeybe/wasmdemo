27ba6a540bef967eabdddf83e3bd7e26d16803b2
Denver Coneybeare <dconeybe@google.com>
2022-12-06 16:30:20 -0500

    download googletest, abseil, and re2 when performing desktop builds

--- external/dep_setup.py
+++ external/dep_setup.py
@@ -1,43 +1,26 @@
-"""
-Syntax: %s <git_commit> <dest_dir>
-"""
-
 from __future__ import annotations
 
 import abc
+import argparse
 from collections.abc import Generator, Mapping, Sequence
 import contextlib
 import dataclasses
+import hashlib
 import io
 import json
+import logging
 import os
 import pathlib
 import shutil
+import sys
 import tempfile
 from typing import Any, IO, Optional
 import zipfile
 
-from absl import app
-from absl import flags
-from absl import logging
-
 import requests
 
 
-DEFAULT_CACHE_DIR = pathlib.Path.home() / ".download_googletest_py_cache"
-
-FLAG_CACHE_DIR = flags.DEFINE_string(
-  name="cache_dir",
-  default=str(DEFAULT_CACHE_DIR),
-  help=f"""The directory into which downloaded files will be cached.
-    Specify the empty string to use the default directory."""
-)
-
-FLAG_CACHE_ENABLED = flags.DEFINE_boolean(
-  name="cache",
-  default=True,
-  help=f"""Whether to save downloaded files into the local cache."""
-)
+DEFAULT_CACHE_DIR = pathlib.Path.home() / ".dep_setup_cache"
 
 
 @dataclasses.dataclass(frozen=True)
@@ -83,6 +66,7 @@ class TransientCacheFile(CacheFile):
     return OpenedFilePathPair(path=self.synthetic_path, f=self.f)
 
   def commit(self) -> OpenedFilePathPair:
+    self.f.seek(0)
     return OpenedFilePathPair(path=self.synthetic_path, f=self.f)
 
   def rollback(self) -> None:
@@ -94,8 +78,7 @@ class TransientCacheFile(CacheFile):
 
 class PersistentCacheFile(CacheFile):
 
-  def __init__(self, git_commit: str, dest_file: pathlib.Path) -> None:
-    self.git_commit = git_commit
+  def __init__(self, dest_file: pathlib.Path) -> None:
     self.dest_file = dest_file
     self.f: IO[bytes] = None
     self.path: pathlib.Path = None
@@ -136,21 +119,29 @@ class PersistentCacheFile(CacheFile):
 
 class Stamp:
 
-  def __init__(self, git_commit: str) -> None:
-    self.git_commit = git_commit
+  def __init__(self, package_name: str, package_version: str) -> None:
+    self.package_name = package_name
+    self.package_version = package_version
 
   def save(self, file_path: pathlib.Path) -> None:
-    data = { "git_commit": str(self.git_commit) }
+    data = {
+      "package_name": str(self.package_name),
+      "package_version": str(self.package_version),
+    }
     with file_path.open("wt", encoding="utf8") as f:
       json.dump(data, f, ensure_ascii=False, indent=2, sort_keys=True)
 
   def __eq__(self, other: Any) -> bool:
     try:
-      other_git_commit = other.git_commit
+      other_package_name = other.package_name
+      other_package_version = other.package_version
     except AttributeError:
       return False
     else:
-      return other_git_commit == self.git_commit
+      return (
+        other_package_name == self.package_name and
+        other_package_version == self.package_version
+      )
 
   def __ne__(self, other: Any) -> bool:
     return not self.__eq__(other)
@@ -165,30 +156,47 @@ class Stamp:
     decoded_json = json.loads(encoded_json)
     if not isinstance(decoded_json, Mapping):
       raise cls.StampDecodeError(f"Decoded JSON should be a map, but got {type(decoded_json)}")
-    git_commit = decoded_json.get("git_commit")
-    if git_commit is None:
-      raise cls.StampDecodeError("git_commit key/value pair is missing")
-    if not isinstance(git_commit, str):
-      raise cls.StampDecodeError(f"git_commit should be a string, but got {type(git_commit)}")
 
-    return cls(git_commit=git_commit)
+    package_name = decoded_json.get("package_name")
+    if package_name is None:
+      raise cls.StampDecodeError("package_name key/value pair is missing")
+    if not isinstance(package_name, str):
+      raise cls.StampDecodeError(f"package_name should be a string, but got {type(package_name)}")
+
+    package_version = decoded_json.get("package_version")
+    if package_version is None:
+      raise cls.StampDecodeError("package_version key/value pair is missing")
+    if not isinstance(package_version, str):
+      raise cls.StampDecodeError(f"package_version should be a string, but got {type(package_version)}")
+
+    return cls(
+      package_name=package_name,
+      package_version=package_version,
+    )
 
   class StampDecodeError(Exception):
     pass
 
 
-class GoogletestDownloader:
+class Downloader:
 
   def __init__(
     self,
-    git_commit: str,
+    package_name: str,
+    package_version: str,
+    download_url: str,
+    expected_sha256: str,
     dest_dir: pathlib.Path,
     cache_dir: Optional[pathlib.Path],
   ) -> None:
-    self.git_commit = git_commit
+    self.package_name = package_name
+    self.package_version = package_version
+    self.download_url = download_url
+    self.expected_sha256 = expected_sha256
     self.dest_dir = dest_dir
     self.cache_dir = cache_dir
-    self.stamp_file = dest_dir / "stamp.json"
+    self.stamp_file = dest_dir / "dep_setup.stamp.json"
+    self.stamp = Stamp(package_name=package_name, package_version=package_version)
 
   def run(self) -> None:
     if self.is_stamp_file_valid():
@@ -199,8 +207,9 @@ class GoogletestDownloader:
       logging.info("Deleting %s", self.dest_dir)
       shutil.rmtree(self.dest_dir)
 
-    with self.download() as googletest_zip_file_path_pair:
-      self.extract(googletest_zip_file_path_pair)
+    with self.download() as zip_file_path_pair:
+      self.verify_sha256(zip_file_path_pair)
+      self.extract(zip_file_path_pair)
 
     self.fixup()
     self.write_stamp_file()
@@ -216,8 +225,7 @@ class GoogletestDownloader:
       logging.info("Validating stamp file %s failed: %s", self.stamp_file, e)
       return False
 
-    expected_stamp = Stamp(git_commit=self.git_commit)
-    if stamp != expected_stamp:
+    if stamp != self.stamp:
       logging.info("Validating stamp file %s failed: stamp does not equal the "
         "expected stamp", self.stamp_file)
       return False
@@ -227,17 +235,15 @@ class GoogletestDownloader:
 
   def write_stamp_file(self) -> bool:
     logging.info("Writing stamp to %s", self.stamp_file)
-    stamp = Stamp(git_commit=self.git_commit)
-    stamp.save(self.stamp_file)
+    self.stamp.save(self.stamp_file)
 
   @contextlib.contextmanager
   def download(self) -> Generator[OpenedFilePathPair, None, None]:
-    cache_dir = self.cache_dir
-    download_dest_file_name = f"googletest_{self.git_commit}.zip"
-    if not cache_dir:
+    download_dest_file_name = f"{self.package_name}_{self.package_version}.zip"
+    if not self.cache_dir:
       download_cache_file = TransientCacheFile(name_hint=download_dest_file_name)
     else:
-      download_dest_file = cache_dir / download_dest_file_name
+      download_dest_file = self.cache_dir / download_dest_file_name
 
       # Use the previously-downloaded file, if it exists
       if download_dest_file.exists():
@@ -246,19 +252,15 @@ class GoogletestDownloader:
           yield OpenedFilePathPair(download_dest_file, f)
           return
 
-      download_cache_file = PersistentCacheFile(
-        git_commit=self.git_commit,
-        dest_file=download_dest_file,
-      )
+      download_cache_file = PersistentCacheFile(download_dest_file)
 
     with download_cache_file as download_cache_file_file_path_pair:
       download_file_path = download_cache_file_file_path_pair.path
       download_file = download_cache_file_file_path_pair.f
-      url = f"https://github.com/google/googletest/archive/{self.git_commit}.zip"
-      logging.info("Downloading %s to %s", url, download_file_path)
+      logging.info("Downloading %s to %s", self.download_url, download_file_path)
 
       session = requests.Session()
-      response = session.get(url, stream=True)
+      response = session.get(self.download_url, stream=True)
       response.raise_for_status()
       content_length = response.headers.get("Content-Length")
       if content_length:
@@ -273,11 +275,33 @@ class GoogletestDownloader:
         download_file.write(chunk)
         downloaded_bytes_count += len(chunk)
 
-      logging.info("Downloaded %s bytes from %s", f"{downloaded_bytes_count:,}", url)
+      logging.info(
+        "Downloaded %s bytes from %s",
+        f"{downloaded_bytes_count:,}",
+        self.download_url,
+      )
 
       committed_file_path_pair = download_cache_file.commit()
       with committed_file_path_pair.f:
         yield committed_file_path_pair
+
+  def verify_sha256(self, zip_file_path_pair: OpenedFilePathPair) -> None:
+    logging.info("Verifying SHA256 hash of %s", zip_file_path_pair.path)
+    hasher = hashlib.sha256()
+    while True:
+      chunk = zip_file_path_pair.f.read(65536)
+      if not chunk:
+        break
+      hasher.update(chunk)
+
+    zip_file_path_pair.f.seek(0)
+
+    actual_sha256 = hasher.hexdigest()
+    if actual_sha256 != self.expected_sha256:
+      raise self.HashMismatch(
+        f"The SHA256 hash of {zip_file_path_pair.path} was "
+        f"{actual_sha256} but expected {self.expected_sha256}"
+      )
 
   def extract(self, zip_file_path_pair: OpenedFilePathPair) -> None:
     if not self.dest_dir.exists():
@@ -323,27 +347,88 @@ class GoogletestDownloader:
   class InvalidDirectoryLayout(Exception):
     pass
 
+  class HashMismatch(Exception):
+    pass
 
-def main(argv: Sequence[str]) -> None:
-  if len(argv) < 2:
-    raise app.UsageError("<git_commit> must be specified")
-  if len(argv) < 3:
-    raise app.UsageError("<dest_dir> must be specified")
-  if len(argv) > 3:
-    raise app.UsageError(f"unexpected argument: {argv[3]}")
 
-  git_commit = argv[1]
-  dest_dir = pathlib.Path(argv[2])
-  cache_dir = pathlib.Path(FLAG_CACHE_DIR.value) \
-    if FLAG_CACHE_ENABLED.value else None
+@dataclasses.dataclass(frozen=True)
+class ParsedArguments:
+  package_name: str
+  package_version: str
+  download_url: str
+  sha256: str
+  dest_dir: pathlib.Path
+  cache_dir: Optional[pathlib.Path]
 
-  downloader = GoogletestDownloader(
-    git_commit=git_commit,
-    dest_dir=dest_dir,
+
+class ArgumentParseError(Exception):
+  pass
+
+
+def parse_arguments(args: Sequence[str]) -> ParsedArguments:
+  arg_parser = argparse.ArgumentParser(
+    prog=os.path.basename(sys.argv[0]),
+  )
+
+  arg_parser.add_argument("package_name")
+  arg_parser.add_argument("package_version")
+  arg_parser.add_argument("download_url")
+  arg_parser.add_argument("sha256")
+  arg_parser.add_argument("dest_dir")
+
+  cache_dir_arg = arg_parser.add_argument(
+    "--cache_dir",
+    help=f"Default: {DEFAULT_CACHE_DIR}",
+  )
+  cache_enabled_arg = arg_parser.add_argument(
+    "--cache_enabled",
+    action="store_true",
+    default=True,
+  )
+  arg_parser.add_argument(
+    "--nocache_enabled",
+    action="store_false",
+    dest=cache_enabled_arg.dest,
+  )
+
+  parse_args_result = arg_parser.parse_args(args[1:])
+
+  if not parse_args_result.cache_enabled:
+    cache_dir = None
+  elif parse_args_result.cache_dir:
+    cache_dir = pathlib.Path(parse_args_result.cache_dir)
+  else:
+    cache_dir = DEFAULT_CACHE_DIR
+
+  return ParsedArguments(
+    package_name=parse_args_result.package_name,
+    package_version=parse_args_result.package_version,
+    download_url=parse_args_result.download_url,
+    sha256=parse_args_result.sha256,
+    dest_dir=pathlib.Path(parse_args_result.dest_dir),
     cache_dir=cache_dir,
   )
+
+
+def main() -> None:
+  logging.basicConfig(
+    level=logging.INFO,
+    format="%(filename)s %(message)s",
+  )
+
+  args = parse_arguments(sys.argv)
+
+  downloader = Downloader(
+    package_name=args.package_name,
+    package_version=args.package_version,
+    download_url=args.download_url,
+    expected_sha256=args.sha256,
+    dest_dir=args.dest_dir,
+    cache_dir=args.cache_dir,
+  )
+
   downloader.run()
 
 
 if __name__ == "__main__":
-  app.run(main)
+  main()
